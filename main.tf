6f06bdea041bc8a3e7274ea602f58d7e22940799
Kyouichi Okumura <tkd.ft.kyouichi@gmail.com>
2025-07-21 02:16:53 +0900

    add variable and output

--- main.tf
+++ main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
 locals {
   ami = {
     Ubuntu_Server_24_04_LTS_HVM_SSD_Volume_Type = "ami-0d1b5a8c13042c939"
@@ -9,14 +20,14 @@ locals {
 }
 
 resource "aws_instance" "example" {
-  ami           = local.ami.Ubuntu_Server_24_04_LTS_HVM_SSD_Volume_Type
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  ami                    = local.ami.Ubuntu_Server_24_04_LTS_HVM_SSD_Volume_Type
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
   user_data_replace_on_change = true
@@ -30,8 +41,8 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
