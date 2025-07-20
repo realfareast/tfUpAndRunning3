b803956fd700aa584fff6a21d04bd68279b52a6e
Kyouichi Okumura <tkd.ft.kyouichi@gmail.com>
2025-07-21 02:03:43 +0900

    Apply terraform fmt and add web server

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "us-east-2"
 }
 
 locals {
@@ -9,9 +9,30 @@ locals {
 }
 
 resource "aws_instance" "example" {
-    ami = local.ami.Ubuntu_Server_24_04_LTS_HVM_SSD_Volume_Type
-    instance_type = "t2.micro"
-    tags = {
-      Name = "terraform-example"
-    }
+  ami           = local.ami.Ubuntu_Server_24_04_LTS_HVM_SSD_Volume_Type
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
+
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
